feec0f688b70a72717c299ed3307e638fd446a48
Yayo Gutierrez <yayobyte@gmail.com>
2025-02-26 20:53:58 -0500

    update favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
+import favicon from './assets/images/symbol.svg'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Set favicon programmatically
+const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (link) {
+  link.href = favicon;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
