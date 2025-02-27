6dbc78f167e12c64590f83c9dfe3cd3b9d175c7a
Yayo Gutierrez <yayobyte@gmail.com>
2025-02-26 20:12:17 -0500

    updated home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Invoices from './modules/Invoices/Invoices';
 import Navigator from './components/ui/navigator/Navigator';
 import styles from './App.module.css';
+import Home from './modules/Home/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <main className={styles.main}>
         <div className={styles.content}>
           <Routes>
-            <Route path="/" element={<h1>Welcome to Calido Viaje</h1>} />
+            <Route path="/" element={<Home />} />
             <Route path="/invoices" element={<Invoices />} />
           </Routes>
         </div>
