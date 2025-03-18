1c6a92bb678167195e2d9b0205e4dd91c3cefcd4
Yayo Gutierrez <yayobyte@gmail.com>
2025-03-18 18:14:55 -0500

    generate payments in invoice

--- src/modules/Invoices/helper.ts
+++ src/modules/Invoices/helper.ts
@@ -1,5 +1,5 @@
 import { formatColombianCurrency } from "../../helpers/currency"
-import { formattedMediumDate } from "../../helpers/date"
+import { formattedMediumDate, formattedDate } from "../../helpers/date"
 import { Invoice } from "../../middleware/types"
 
 export const mapInvoiceInfo = (invoice: Invoice) => {
@@ -23,6 +23,14 @@ export const mapInvoiceInfo = (invoice: Invoice) => {
                 item_total: formatColombianCurrency(partialTotal)
             })
         }),
+        payments: invoice.payments?.map((payment) => {
+            return ({
+                payment_date: formattedDate(payment.paymentDate),
+                payment_amount: formatColombianCurrency(payment.amount),
+            })
+        }),
+        total_payments: formatColombianCurrency(invoice.totalPaid || 0),
+        pending_amount: formatColombianCurrency(invoice.balanceDue || 0),
         total: formatColombianCurrency(total),
     }
 }
